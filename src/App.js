ba3f76bd9e8ee982fc03733099e1139287a82599
Scot Goodhart <scot.goodhart@gmail.com>
2017-11-16 09:19:15 -0600

    welcome page hack

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css'
 
 import Buttons from './components/Buttons'
 import CardDeck from './components/CardDeck'
-import Movember from './components/Movember'
 
 const MUSTACHES = [
             {   
@@ -54,10 +53,15 @@ const MUSTACHES = [
 class App extends Component {
   constructor() {
     super()
+    this.handleWelcomeClick = this.handleWelcomeClick.bind(this);
     this.state = {
       cards: MUSTACHES,
-      isHome: false
+      isHome: true
     }
+  }
+
+  handleWelcomeClick() {
+    this.setState({isHome: false});
   }
 
   componentDidUpdate() {
@@ -74,7 +78,7 @@ class App extends Component {
     const isHome = this.state.isHome;
     let page = null;
     if(isHome){
-        page =  <Movember />;
+            page =  <WelcomePage onClick={this.handleWelcomeClick} />;
     } else {
         page = <div className="App"><CardDeck cards={this.state.cards} shiftCard={this.shiftCard.bind(this)} /><Buttons cards={this.state.cards} shiftCard={this.shiftCard.bind(this)} /></div>;
     }
@@ -85,5 +89,20 @@ class App extends Component {
     )
   }
 }
+
+function WelcomePage(props) {
+    return (
+        <div className="Movember" >
+        <h3> Welcome to Movember! </h3> 
+        <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Phasellus non feugiat enim.Proin egestas aliquet erat et aliquet.Vivamus imperdiet suscipit felis, id laoreet ligula feugiat id.In vitae purus pellentesque, venenatis mauris ac, sagittis lectus.Aliquam rutrum ex sit amet tortor pretium condimentum.Vestibulum auctor eleifend sagittis.Quisque pulvinar nisl non odio luctus elementum.Aliquam condimentum nisi a nunc feugiat scelerisque.Morbi nec tempus enim.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Aenean vel metus rutrum, ullamcorper purus congue, congue orci.</p>
+        <p>Cras pharetra dolor justo, vitae cursus mi tincidunt a.In viverra faucibus enim, ac ultrices neque iaculis nec.In semper metus condimentum quam ultricies, eu rhoncus nisl volutpat.Proin sit amet neque vitae felis elementum posuere.Vivamus blandit felis at laoreet facilisis.Etiam aliquam elit quam, non mollis turpis fringilla sit amet.Donec quam erat, iaculis sit amet dapibus nec, eleifend nec massa.Donec vitae elit vestibulum, cursus sem quis, fermentum lacus.Sed sed sollicitudin augue.Quisque mauris magna, venenatis a erat ut, fermentum varius purus.Vestibulum sed sagittis tellus.Suspendisse nec metus massa.Pellentesque sit amet massa eget tellus lacinia bibendum vitae quis magna.Phasellus malesuada, diam sit amet tristique euismod.</p></div>
+        <button onClick={props.onClick}>
+        To the Staches!
+      </button>        
+        </div>
+
+    );
+  }
 
 export default App;
