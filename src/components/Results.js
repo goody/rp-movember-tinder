2405dafbabb3ff39cdaa969009ea361826726989
Scot Goodhart <scot.goodhart@gmail.com>
2017-11-16 22:02:00 -0600

    adding results

--- src/components/Results.js
+++ src/components/Results.js
@@ -9,33 +9,47 @@ export default class Results extends Component {
     }
   }
 
-  componentDidMount() {
-    /* Create reference to messages in Firebase Database */
-    let messagesRef = fire.database().ref('votes').orderByKey().limitToLast(100);
-    debugger;
-    messagesRef.on('child_added', snapshot => {
-        /* Update React state when message is added at Firebase Database */
-        let vote = { text: snapshot.val(), id: snapshot.key };
-        this.setState({ results: [vote].concat(this.state.results) });
-      })
-    let totals = Object.values(messagesRef).reduce(function(sums,value){
-        if(!sums.hasOwnProperty(value.person)){sums[value.person] = {yes:0,no:0}};
-         sums[value.person] = { yes: sums[value.person].yes += (value.vote === 'YES') ?  1 : 0,
-                               no: sums[value.person].no += (value.vote === 'NO') ?  1 : 0 }
-         return sums;
-      },{});
-      this.setState({results: totals});
+  count = 0;
 
-/*     messagesRef.on('child_added', snapshot => {
-      /* Update React state when message is added at Firebase Database 
-      let message = { text: snapshot.val(), id: snapshot.key };
-      this.setState({ results: [message].concat(this.state.messages) });
-    }) */
+  componentDidMount() {
+    this.timerID = setInterval(
+        () => {
+    //get data and set state.results.
+    let self = this;
+    fire.database().ref('votes').once('value')
+        .then(snapshot => {
+            let obj = snapshot.val();
+            let totals = Object.values(obj).reduce(function(sums,value){
+                if(!sums.hasOwnProperty(value.person)){sums[value.person] = {yes:0,no:0}};
+                sums[value.person] = { 
+                        yes: sums[value.person].yes += (value.vote === 'YES') ?  1 : 0,
+                        no: sums[value.person].no += (value.vote === 'NO') ?  1 : 0 
+                    }
+                return sums;
+            },{});
+            
+
+            self.setState({results: totals});        
+    }),
+       1000 });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
   }
 
   render() {
-    //let {results} = this.state.results
 
-    return <div>Results</div>
+    return <div>
+        <h3>Results</h3>
+        <ul>
+          { /* Render the list of messages */
+            Object.keys(this.state.results).map( key => 
+                <li>{key} | {this.state.results[key].yes}</li>
+            )
+          }
+        </ul>
+
+    </div>
   }
 }
